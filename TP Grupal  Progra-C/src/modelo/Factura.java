dc11cb18bb4f7516ec5a7d0809ca05ae64bef088
IanaiGribman <ianaigribman@gmail.com>
2025-10-05 20:28:39 -0300

    Añadida creacion de la factura

--- TP Grupal  Progra-C/src/modelo/Factura.java
+++ TP Grupal  Progra-C/src/modelo/Factura.java
@@ -7,10 +7,10 @@ public class Factura {
 	private Habitacion habitacion;
 	private ArrayList<IMedico> medicosConsultados;
 	private LocalDate fechaIngreso;
-	private LocalDate fechaSalida;
 	private String nombrePaciente;
 	private int nroFactura;
 	private static int sigNum = 1;
+	private int cantDias;
 	
 	/**
 	 * 
@@ -23,13 +23,69 @@ public class Factura {
 	public Factura(String nombrePaciente, LocalDate fechaIngreso, ArrayList<IMedico> medicosConsultados, Habitacion habitacion,  int cantDias)
 	{
 		this.fechaIngreso = fechaIngreso;
-		this.fechaSalida = fechaIngreso.plusDays(cantDias);
 		this.nombrePaciente = nombrePaciente;
 		this.habitacion = habitacion;
+		this.cantDias = cantDias;
 		this.medicosConsultados = medicosConsultados;
 		this.nroFactura = sigNum;
 		sigNum++;
 	}
+
+	@Override
+	public String toString() {
+		String texto = "";
+		double costoTotal = 0;
+		texto = texto.concat("N° Factura: " + this.nroFactura + "\n");
+		texto = texto.concat("Nombre Paciente: " + this.nombrePaciente + "\n");
+		texto = texto.concat("Fecha Ingreso: " + this.fechaIngreso.toString() + "\n");
+		texto = texto.concat("Fecha Egreso: " + getFechaEgreso().toString() + "\n");
+		texto = texto.concat("Cantidad de dias: " + this.cantDias + "\n");
+		if (this.habitacion != null) {
+			double costoHabitacion = habitacion.getCostoTotal(this.cantDias);
+			costoTotal += costoHabitacion;
+			texto = texto.concat("Habitacion tipo: " + habitacion.getTipoHabitacion() + "         Costo:         $" + costoHabitacion + "\n");
+		}
+		texto = texto.concat("");
+		for (IMedico medico : medicosConsultados) {
+			double honorario = medico.getHonorario();
+			costoTotal += honorario;
+			texto = texto.concat("Nombre Médico: " + medico.getNombre() + "       Especialidad: " + medico.getEspecialidad() + "      Subtotal:     $" + honorario + "\n");
+		}
+		texto = texto.concat("                                         Total:       $" + costoTotal);
+		return texto;
+	}
+
+	public Habitacion getHabitacion() {
+		return habitacion;
+	}
+
+	public ArrayList<IMedico> getMedicosConsultados() {
+		return medicosConsultados;
+	}
+
+	public LocalDate getFechaIngreso() {
+		return fechaIngreso;
+	}
+	
+	public LocalDate getFechaEgreso() {
+		return fechaIngreso.plusDays(cantDias);
+	}
+
+	public String getNombrePaciente() {
+		return nombrePaciente;
+	}
+
+	public int getNroFactura() {
+		return nroFactura;
+	}
+
+	public int getCantDias() {
+		return cantDias;
+	}
+	
+	
+	
+	
 }
 	
 	
