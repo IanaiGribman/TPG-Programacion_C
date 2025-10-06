f58d23d096b70a9a508552d4ae5b7424069b0c50
IanaiGribman <ianaigribman@gmail.com>
2025-10-05 21:12:08 -0300

    documentado codigo de habitacion

--- TP Grupal  Progra-C/src/modelo/Habitacion.java
+++ TP Grupal  Progra-C/src/modelo/Habitacion.java
@@ -4,20 +4,34 @@ public abstract class Habitacion {
 	private static double costoAsignacion;
 	private int capacidad,ocupacion;
 
+	/**
+	 * 
+	 * @param capacidad debe ser mayor a 0
+	 */
 	public Habitacion(int capacidad) {
 		super();
 		this.capacidad = capacidad;
 		this.ocupacion = 0;
 	}
-
+	/**
+	 * 
+	 * @return el tipo de la habitacion
+	 */
 	public abstract String getTipoHabitacion();
 
+	/**
+	 * precondicion: se han asignado valores al costo de la habitacion y a la asignacion
+	 * @param dias debe ser mayor a 0
+	 * @return costo total para los dias internados
+	 */
 	public abstract double getCostoTotal(int dias);
 
 	public static double getCostoAsignacion() {
 		return costoAsignacion;
 	}
-
+	/**
+	 * @param costoAsignacion el costo debe ser positivo
+	 */
 	public static void setCostoAsignacion(double costoAsignacion) {
 		Habitacion.costoAsignacion = costoAsignacion;
 	}
@@ -30,6 +44,10 @@ public abstract class Habitacion {
 		return ocupacion;
 	}
 
+	/**
+	 * 
+	 * @param ocupacion debe ser un numero positivo menor o igual a la capacidad
+	 */
 	public void setOcupacion(int ocupacion) {
 		this.ocupacion = ocupacion;
 	}
