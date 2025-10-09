20aad619578268fbc7c32f7d718049a68eebee83
IanaiGribman <ianaigribman@gmail.com>
2025-10-08 23:42:51 -0300

    Modificada la clase Factura para ser inmutable y creada la clase ConsultaMedicaFactura para ordenar mas facilmente la factura

--- TP Grupal  Progra-C/src/modelo/Factura.java
+++ TP Grupal  Progra-C/src/modelo/Factura.java
@@ -1,34 +1,47 @@
 package modelo;
 
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
-
 import modelo.habitaciones.Habitacion;
 
+/**
+ * todos los datos de una factura son inmutables
+ */
 public class Factura {
-	private Habitacion habitacion;
-	private ArrayList<IMedico> medicosConsultados;
-	private LocalDate fechaIngreso;
+	private ArrayList<ConsultaMedicaFactura> consultasMedicas;
+	private LocalDate fechaIngreso, fechaEgreso;
 	private String nombrePaciente;
+	private String tipoHabitacion;
+	private double costoHabitacion;
 	private int nroFactura;
 	private static int sigNum = 1;
 	private int cantDias;
-	
+
 	/**
-	 * 
-	 * @param nombrePaciente no puede ser null
-	 * @param fechaIngreso no puede ser null
-	 * @param medicosConsultados no puede ser null
-	 * @param habitacion
-	 * @param cantDias
+	 * @param medicosConsultados distinto de null y al menos un medico
+	 * @param habitacion la habitacion puede ser null, de serlo el paciente no se interno
+	 * @param nombrePaciente distinto de null
+	 * @param fechaIngreso distinto de null
+	 * @param fechaEgreso distindo de null 
 	 */
-	public Factura(String nombrePaciente, LocalDate fechaIngreso, ArrayList<IMedico> medicosConsultados, Habitacion habitacion,  int cantDias)
-	{
+	public Factura(ArrayList<IMedico> medicosConsultados, Habitacion habitacion, String nombrePaciente,
+			LocalDate fechaIngreso, LocalDate fechaEgreso) {
 		this.fechaIngreso = fechaIngreso;
+		this.fechaEgreso = fechaEgreso;
+		this.cantDias = (int) ChronoUnit.DAYS.between(fechaIngreso, fechaEgreso);
 		this.nombrePaciente = nombrePaciente;
-		this.habitacion = habitacion;
-		this.cantDias = cantDias;
-		this.medicosConsultados = medicosConsultados;
+		if (habitacion != null) {
+			this.tipoHabitacion = habitacion.getTipoHabitacion();
+			this.costoHabitacion = habitacion.getCostoTotal(cantDias);
+		}
+		else {
+			this.tipoHabitacion = null;
+			this.costoHabitacion = 0;
+		}
+		for (IMedico medico : medicosConsultados) {
+			this.consultasMedicas.add(new ConsultaMedicaFactura(medico));
+		}
 		this.nroFactura = sigNum;
 		sigNum++;
 	}
@@ -37,40 +50,30 @@ public class Factura {
 	public String toString() {
 		String texto = "";
 		double costoTotal = 0;
-		texto = texto.concat("N� Factura: " + this.nroFactura + "\n");
+		texto = texto.concat("Nro Factura: " + this.nroFactura + "\n");
 		texto = texto.concat("Nombre Paciente: " + this.nombrePaciente + "\n");
 		texto = texto.concat("Fecha Ingreso: " + this.fechaIngreso.toString() + "\n");
-		texto = texto.concat("Fecha Egreso: " + getFechaEgreso().toString() + "\n");
+		texto = texto.concat("Fecha Egreso: " + this.fechaEgreso.toString() + "\n");
 		texto = texto.concat("Cantidad de dias: " + this.cantDias + "\n");
-		if (this.habitacion != null) {
-			double costoHabitacion = habitacion.getCostoTotal(this.cantDias);
-			costoTotal += costoHabitacion;
-			texto = texto.concat("Habitacion tipo: " + habitacion.getTipoHabitacion() + "         Costo:         $" + costoHabitacion + "\n");
+		if (this.tipoHabitacion != null) {
+			costoTotal += this.costoHabitacion;
+			texto = texto.concat("Habitacion tipo: " + this.tipoHabitacion + "         Costo:         $"
+					+ this.costoHabitacion + "\n");
 		}
-		texto = texto.concat("");
-		for (IMedico medico : medicosConsultados) {
-			double honorario = medico.getHonorario() * 1.2;
-			costoTotal += honorario;
-			texto = texto.concat("Nombre M�dico: " + medico.getNombre() + "       Especialidad: " + medico.getEspecialidad() + "      Subtotal:     $" + honorario + "\n");
+		for (ConsultaMedicaFactura consulta : this.consultasMedicas) {
+			costoTotal += consulta.getHonorario();
+			texto = texto.concat(consulta.toString() + "\n");
 		}
 		texto = texto.concat("                                         Total:       $" + costoTotal);
 		return texto;
 	}
 
-	public Habitacion getHabitacion() {
-		return habitacion;
-	}
-
-	public ArrayList<IMedico> getMedicosConsultados() {
-		return medicosConsultados;
-	}
-
 	public LocalDate getFechaIngreso() {
 		return fechaIngreso;
 	}
-	
+
 	public LocalDate getFechaEgreso() {
-		return fechaIngreso.plusDays(cantDias);
+		return fechaEgreso;
 	}
 
 	public String getNombrePaciente() {
@@ -84,8 +87,18 @@ public class Factura {
 	public int getCantDias() {
 		return cantDias;
 	}
-}
-	
-	
-	
 
+	public ArrayList<ConsultaMedicaFactura> getConsultasMedicas() {
+		return consultasMedicas;
+	}
+
+	public String getTipoHabitacion() {
+		return tipoHabitacion;
+	}
+
+	public double getCostoHabitacion() {
+		return costoHabitacion;
+	}
+	
+	
+}
