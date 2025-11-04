63c96709791fc970c8fe49e2d96626bd4adcfdef
Valentina A.F. <valenvick23@gmail.com>
2025-11-04 11:01:59 -0300

    Asociado y controlador simulacion

--- TP Grupal  Progra-C/src/modelo/Asociado.java
+++ TP Grupal  Progra-C/src/modelo/Asociado.java
@@ -1,5 +1,27 @@
 package modelo;
 
-public class Asociado {
+public class Asociado implements IPersona, Runnable
+{
+	private String dni;
+	private String nombre;
+	private Ambulancia ambulancia;
+	
+	
+	public Asociado(String dni, String nombre, Ambulancia ambulancia) {
+		super();
+		this.dni = dni;
+		this.nombre = nombre;
+		this.ambulancia = ambulancia;
+	}
 
+	@Override
+	public String getNombre() {
+		return this.nombre;
+	}
+	
+	@Override
+	public String getDni() {
+		return this.dni;
+	}
+	
 }
