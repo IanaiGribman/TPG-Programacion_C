39cf4145ba7c44b11873267215142557e08d89b1
IanaiGribman <ianaigribman@gmail.com>
2025-11-14 12:11:01 -0300

    mejorados metodos tiempo muerto

--- TP Grupal  Progra-C/src/util/Util.java
+++ TP Grupal  Progra-C/src/util/Util.java
@@ -5,12 +5,15 @@ import java.util.Random;
 public class Util {
 	private static Random random = new Random();
 	
-	public static void tiempoMuerto() throws InterruptedException{
-		Thread.sleep(1000 + random.nextInt(1000));
+	public static void tiempoMuerto(int base, int extra) throws InterruptedException {
+		Thread.sleep(base + random.nextInt(extra));
 	}
-	
 	public static void tiempoMuerto(int tiempo) throws InterruptedException{
-		Thread.sleep(tiempo);
+		tiempoMuerto(tiempo,0);
+	}
+
+	public static void tiempoMuerto() throws InterruptedException{
+		tiempoMuerto(1000,1000);
 	}
 	
 	public static int numeroAleatorio(int base, int max) {
